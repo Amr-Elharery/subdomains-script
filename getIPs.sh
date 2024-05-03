970fcb1ff75b66501f621da37c83d224524f78fb
Amr Mero <87432451+Amr-Elharery@users.noreply.github.com>
2024-05-03 12:32:44 -0400

    Add the script file

---
+++ getIPs.sh
@@ -0,0 +1,48 @@
+(
+echo "Prepared by"
+echo "=================================="
+echo "===========Amr Elharery==========="
+echo "===========Ahmed Megzoo==========="
+echo "=================================="
+
+echo "  \ "
+echo "   \ "
+echo "      .--."
+echo "     |o_o |"
+echo "     |:/ |"
+echo "    //   \ "
+echo "   (|     | )"
+echo "  /'_   /\`"
+echo "  _)=(/"
+
+
+echo "Domain you want to get subdomain"
+read  domain
+
+mkdir output
+cd output
+wget $domain.com -O $domain.html
+
+cat $domain.html | grep "href=" | cut -d "/" -f3 | cut -d '"' -f2 | grep $domain.com | sort -u > $domain-IPs.txt
+
+urls=`cat $domain-IPs.txt`
+
+for url in $urls
+do
+	if [ $url ]
+	then
+		echo $url Has IP
+		echo $url Has IP >> $domain-result.txt
+		
+		host $url | grep "has address" | cut -d " " -f4
+		host $url | grep "has address" | cut -d " " -f4 >> $domain-result.txt
+		echo ------
+		echo ------ >> $domain-result.txt
+	else
+		echo "Error, may be there are no sub domains."
+		exit
+	fi
+done
+
+echo Saved in $domain-result.txt
+)
